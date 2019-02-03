0d371b444027068a09940e82646cb3675a95d5a4
James Will <jwillfiji@gmail.com>
2019-02-03 15:34:12 -0600

    Create pins.py

---
+++ src/pins.py
@@ -0,0 +1,25 @@
+from utils import config
+
+
+# When Someone Reacts with a Pushpin Emote
+async def on_pin(reaction, bot):
+
+    # Check for Needed Reacts and If Message is Pinned Already
+    if reaction.count < 4 and not reaction.message.pinned:
+        return
+
+    # Define Allowed Channels
+    channels = []
+    for channel_id in config.ALLOWED_PIN_CHANNELS:
+        channels.append(bot.get_channel(channel_id))
+
+    # Check for Allowed Channels
+    if reaction.message.channel not in channels:
+        return
+
+    current_pinned = await bot.pins_from(reaction.message.channel)
+    if len(current_pinned) < 50:
+        await bot.pin_message(reaction.message)
+    else:
+        await bot.send_message(reaction.message.channel, "I cannot pin the message. We have reached the maximum number "
+                                                         "of pins comrades!")
