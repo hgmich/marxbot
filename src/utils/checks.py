f25396ee5fb750a5a2d4a375c0af4e442b3b9dc5
James Will <jwillfiji@gmail.com>
2019-02-03 15:42:28 -0600

    Create checks.py

---
+++ src/utils/checks.py
@@ -0,0 +1,48 @@
+from discord.ext import commands
+import discord.utils
+from utils import config
+
+
+# Check for Role
+def check_role(ctx, check):
+    msg = ctx.message
+    ch = msg.channel
+    author = msg.author
+
+    if ch.is_private:
+        return False
+
+    role = discord.utils.find(check, author.roles)
+    return role is not None
+
+
+# Check for Staff
+def is_staff():
+    def predicate(ctx):
+        return check_role(ctx, lambda r: r.id in (config.ROLE_ID_ADMIN, config.ROLE_ID_MOD))
+
+    return commands.check(predicate)
+
+
+# Check for Admins
+def is_admin():
+    def predicate(ctx):
+        return check_role(ctx, lambda r: r.id == config.ROLE_ID_ADMIN)
+
+    return commands.check(predicate)
+
+
+# Check for Moderators
+def is_mod():
+    def predicate(ctx):
+        return check_role(ctx, lambda r: r.id == config.ROLE_ID_MOD)
+
+    return commands.check(predicate)
+
+
+# Check for Comrade/Member
+def is_member():
+    def predicate(ctx):
+        return check_role(ctx, lambda r: r.id == config.ROLE_ID_MEMBER)
+
+    return commands.check(predicate)
