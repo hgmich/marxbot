2f5d1be55dabae2010fb7c17757f92598e272046
James Will <jwillfiji@gmail.com>
2019-02-03 15:43:12 -0600

    Create emojis.py

---
+++ src/utils/emojis.py
@@ -0,0 +1,12 @@
+from discord import Emoji
+
+
+# Create Emoji
+def create_emoji(_name, _id):
+    return Emoji(name=_name, id=_id, server=338594020101980160)
+
+
+# Emoji List
+bookmark_emoji = "\U0001f516"
+pushpin_emoji = "\U0001f4cc"
+paperclip_emoji = "\U0001f4ce"
