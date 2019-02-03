8c0f188a4324bdac140cb449a4eea73a9ddf5f71
James Will <jwillfiji@gmail.com>
2019-02-03 15:32:29 -0600

    Update main.py

--- src/main.py
+++ src/main.py
@@ -2,104 +2,145 @@ import discord
 from discord.ext import commands
 import logging
 import asyncio
+from utils import config
+from utils.emojis import *
+from clipboard import on_clip
+from bookmark import on_bookmark
+from pins import on_pin
+
 
 # BOT LOGGING
-
 logger = logging.getLogger('discord')
 logger.setLevel(logging.WARNING)
 handler = logging.FileHandler(filename='marxbot.log', encoding='utf-8', mode='w')
 handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
 logger.addHandler(handler)
 
+# Bot Description
 description = '''Famed Economic Theorist and Supporter of the Working Peoples' Servers.'''
 
-# this specifies what extensions to load when the bot starts up
+# This specifies what extensions to load when the bot starts up
 startup_extensions = ["general", "roles", "music", "wiki", "staff", "tweet_watch"]
 
+# Setup Bot
 bot = commands.Bot(command_prefix='!', description="Karl Marxbot.")
 
 
 @bot.event
 async def on_ready():
-    print('Logged in as')
-    print(bot.user.name)
-    print(bot.user.id)
-    print('------')
-    await bot.change_presence(game=discord.Game(name='Communism Simulator'))
+    print('..... Marxbot has started .....')
+    await bot.change_presence(game=discord.Game(name='Communism Simulator 2.5'))
 
 
 # Welcome Message
 @bot.event
 async def on_member_join(member):
-    # Define Channels
-    # channel_general = bot.get_channel('338594020101980160')
-    channel_lobby = bot.get_channel('359182653934665749')
-    channel_info = bot.get_channel('356968473819348994')
-    channel_updates = bot.get_channel('356972304019881984')
 
-    # Get Probie Role
-    probie_role = discord.utils.get(member.server.roles, name='Psyops')
+    # Define Channels
+    channel_join_leave = bot.get_channel(config.CHANNEL_ID_JOINLEAVE)
+    channel_lobby = bot.get_channel(config.CHANNEL_ID_LOBBY)
+    channel_welcome = bot.get_channel(config.CHANNEL_ID_WELCOME)
+
+    # Get Probationary Role
+    probie_role = discord.utils.get(member.server.roles, id=config.ROLE_ID_DEFAULT)
     await bot.add_roles(member, probie_role)
 
-    wmsg = "Hello {0.mention}, and welcome to LeftDisc, a leftist Discord server. We thank you for " \
-           "joining, but first some housekeeping. You'll notice that you can only view the {1.mention}, along with " \
-           "{2.mention} (the rules channel) and {3.mention} (the updates and announcements channel). " \
-           "This is because you are considered a **Psyop**, a probationary member. To protect member privacy, as well " \
-           "as prevent spam and trolling from unsavory characters, all members go through a verification process. " \
-           "Please let us know how you heard about LeftDisc, and provide some links to your social media accounts in " \
-           "the {1.mention} channel. If you are uncomfortable doing so, please contact a staff member to discuss. " \
-           "**If you fail to provide details to verify you within 24 hours, you will be kicked from the server**. " \
-           "In the meantime, please read the {2.mention} channel and familiarize yourself with our purpose and rules. " \
-           "Check out the {3.mention} channel for updates and announcements. Thanks, and again, welcome to LeftDisc!" \
-           "".format(member, channel_lobby, channel_info, channel_updates)
-
-    wpm = "Hello comrade, and welcome to LeftDisc, a server, a home, for leftists. A few things to note: " \
-          "Take a look at the {0.mention} channel; it lists what we are about as well as the rules. " \
-          "Important news will be posted in {1.mention}. Again, thank you for joining the server, and the " \
-          "struggle. Please check {2.mention} for more information.".format(channel_info, channel_updates, channel_lobby)
-
-    # Send Welcome Message to the Probie Lobby
-    # await bot.send_message(channel_lobby, wmsg)
+    # Setup Welcome DM
+    wmsg = "Hello {0.mention}, and welcome to LeftDisc, a fun, safe, inclusive, queer-friendly social community for " \
+           "leftists. Thanks for joining, but first some housekeeping. You'll notice that you can only view the " \
+           "{1.mention}, along with {2.mention} (the rules channel). This is because you are considered a **Psyop**, " \
+           "a probationary member. To protect member privacy, as well as prevent spam and trolling from unsavory " \
+           "characters, all members go through a verification process. \n\n Please ping the staff using `@mods` and " \
+           "once contacted, share a bit about your political leanings and some social media links (e.g. Twitter, " \
+           "Mastodon, Reddit). **If you fail to provide details to verify yourself within 24 hours, you will be " \
+           "kicked from the server**. In the meantime, please read the {2.mention} channel and familiarize yourself " \
+           "with our rules. Thanks, and again, welcome to LeftDisc!" \
+           "".format(member, channel_lobby, channel_welcome)
 
     # Send Private Welcome Message
     await bot.send_message(member, wmsg)
+
+    # Setup Alert Message
+    alert_msg = "A new user, {0.mention}, has joined the server.".format(member)
+
+    # Post Alert in Join-Leave Channel
+    await bot.send_message(channel_join_leave, alert_msg)
+
+
+# Leave Alert
+@bot.event
+async def on_member_remove(member):
+    # Define Channel
+    channel_join_leave = bot.get_channel(config.CHANNEL_ID_JOINLEAVE)
+
+    # Setup Alert Message
+    display = member.display_name
+    user_num = member.name + "#" + member.discriminator
+    user_id = member.id
+    alert_msg = "{0.mention} has left the server. **Display**: ".format(member) + display + ", **User**: " + user_num \
+                + ", **UserID**: " + user_id
+
+    # Post Alert in Join-Leave Channel
+    await bot.send_message(channel_join_leave, alert_msg)
 
 
 # Check Messages
 @bot.event
 async def on_message(message):
 
+    # Check for Counting Message & Prevent Bot from Checking Own Messages for Endless Loop
+    if message.channel.id == config.CHANNEL_ID_COUNTING and message.author != bot.user:
+
+        new_number = message.content
+
+        # Is the message strictly a number?
+        if new_number.isdigit():
+            # Is it the next number?
+            if int(new_number) == (config.CURRENT_COUNT + 1):
+                # Increase the Count
+                config.CURRENT_COUNT = (config.CURRENT_COUNT + 1)
+            else:
+                config.CURRENT_COUNT = 0
+                await bot.send_message(message.channel, "**ALERT**: Sorry, that number is not correct! Start over at 1.")
+        else:
+            config.CURRENT_COUNT = 0
+            await bot.send_message(message.channel, "**ALERT**: Sorry, numbers only! Start over at 1.")
+
     # Process the rest of the Commands
     await bot.process_commands(message)
 
 
-# Auto-Pin on Pushpin Reactions in Shitposting Channels
+# Handle Reacts (Pin, Clip, Bookmark
 @bot.event
 async def on_reaction_add(reaction, user):
+    if reaction.emoji == bookmark_emoji:
+        await on_bookmark(reaction, user, bot)
+    elif reaction.emoji == paperclip_emoji:
+        await on_clip(reaction, user, bot)
+    elif reaction.emoji == pushpin_emoji:
+        await on_pin(reaction, bot)
 
-    # Define Allowed Channels (Shitposting, Queer Shitposting, Nice Town, Health & Fitness, Queer)
-    channels = []
-    channels.append(bot.get_channel('386609440863813642'))
-    channels.append(bot.get_channel('361260914164498433'))
-    channels.append(bot.get_channel('425809470623318026'))
-    channels.append(bot.get_channel('449022450806554635'))
-    channels.append(bot.get_channel('359468983499489282'))
 
-    # Check if the reaction is pushpin
-    if str(reaction.emoji) == '\N{PUSHPIN}' and reaction.count >= 4 and not reaction.message.pinned and reaction.message.channel in channels:
-        current_pinned = await bot.pins_from(reaction.message.channel)
-        if len(current_pinned) < 50:
-            await bot.pin_message(reaction.message)
-        else:
-            await bot.send_message(reaction.message.channel, "I cannot pin the message. We have reached the maximum "
-                                                             "number of pins comrades!")
-
-# Goodbye Message
+# Remove Reaction
 # @bot.event
-# async def on_member_remove(member):
-#    channel = bot.get_channel('108369515502411776')
-#    fmt = ":wave: Goodbye {0}, we're sad to see you go!".format(member.name)
-#    await bot.send_message(channel, fmt)
+# async def on_reaction_remove(reaction, user):
+#    # delete message in #pin_board on removal of pushpin emoji
+#    if reaction.emoji == paperclip_emoji:
+#        await remove_clip(reaction.message, bot)
+
+
+async def save_count():
+    await bot.wait_until_ready()
+    while not bot.is_closed:
+
+        updated = config.update_counting()
+
+        if updated:
+            print("Counting stats have been updated.")
+        else:
+            print("ERROR: Counting stats could not be updated.")
+
+        await asyncio.sleep(600)  # task runs every 600 seconds
 
 
 if __name__ == "__main__":
@@ -112,4 +153,5 @@ if __name__ == "__main__":
             if extension == 'tweet_watch':
                 raise e
 
-    bot.run('tokenhere')
+    bot.loop.create_task(save_count())
+    bot.run(config.BOT_TOKEN)
