4a94cc0d18e4c4a169e38777da6c396d3070c643
James Will <jwillfiji@gmail.com>
2019-02-16 22:49:04 -0600

    Update clipboard.py

--- src/clipboard.py
+++ src/clipboard.py
@@ -12,42 +12,7 @@ async def on_clip(reaction, user, bot):
     # The message
     msg = reaction.message
 
-    # Check for Message Already In
-    if config.on_clipboard(msg.id):
-        config.increase_clip(msg.id)
-        return
-
-    # Set the Clipboard Channel
-    clipboard = bot.get_channel(config.CHANNEL_ID_CLIPBOARD)
-
-    # have to add a check here to make sure its not the pin board itself
-    if msg.channel == clipboard:
-        return
-
-    # Check if Message Author is in "No Clip" List
-    if msg.author in config.get_noclip_members():
-        return
-
-    # Setup Embed
-    embed = discord.Embed(colour=discord.Colour(0xca0003), description=msg.content, timestamp=datetime.now())
-
-    if msg.embeds:
-        data = msg.embeds[0]
-        if data.type == 'image':
-            embed.set_image(url=data.url)
-
-    if msg.attachments:
-        file = msg.attachments[0]
-        if file['url'].lower().endswith(('png', 'jpeg', 'jpg', 'gif', 'webp')):
-            embed.set_image(url=file['url'])
-        else:
-            embed.add_field(name='Attachment', value='[' + file['url'] + '](' + file['filename'] + ')', inline=False)
-
-    embed.set_author(name=msg.author.display_name, icon_url=msg.author.avatar_url)
-
-    clip_msg = await bot.send_message(clipboard, content="📋 - from {0.mention}".format(msg.channel), embed=embed)
-
-    config.add_clipboard(msg.id, clip_msg.id)
+    await clip_message(msg, bot)
 
 
 async def remove_clip(reaction, bot):
@@ -59,3 +24,53 @@ async def remove_clip(reaction, bot):
     if config.on_clipboard(msg.id):
         config.decrease_clip(msg.id)
         return
+
+
+async def clip_message(message, bot):
+
+    # Check for Message Already On Clipboard
+    if config.on_clipboard(message.id):
+        config.increase_clip(message.id)
+        return
+
+    # Set the Clipboard Channel
+    clipboard = bot.get_channel(config.CHANNEL_ID_CLIPBOARD)
+
+    # have to add a check here to make sure its not the pin board itself
+    if message.channel == clipboard:
+        return
+
+    # Check if Message Author is in "No Clip" List
+    if message.author in config.get_noclip_members():
+        return
+
+    # Get URL to Post
+    server_id = message.server.id
+    channel_id = message.channel.id
+    message_id = message.id
+    embed_url = "https://discordapp.com/channels/" + server_id + "/" + channel_id + "/" + message_id
+
+    # Setup Embed
+    embed = discord.Embed(colour=discord.Colour(0xca0003), description="[View Original Post](" + embed_url + ")",
+                          timestamp=datetime.now())
+
+    # Add Message Content Field
+    embed.add_field(name='Message', value=message.content, inline=False)
+
+    if message.embeds:
+        data = message.embeds[0]
+        if data.type == 'image':
+            embed.set_image(url=data.url)
+
+    if message.attachments:
+        file = message.attachments[0]
+        if file['url'].lower().endswith(('png', 'jpeg', 'jpg', 'gif', 'webp')):
+            embed.set_image(url=file['url'])
+        else:
+            embed.add_field(name='Attachment', value='[' + file['url'] + '](' + file['filename'] + ')', inline=False)
+
+    embed.set_author(name=message.author.display_name, icon_url=message.author.avatar_url)
+
+    clip_msg = await bot.send_message(clipboard, content="📋 - from {0.mention}".format(message.channel), embed=embed)
+
+    config.add_clipboard(message.id, clip_msg.id)
