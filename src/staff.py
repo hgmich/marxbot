d28b981d46088c11567922714672fee4bef85c1b
James Will <jwillfiji@gmail.com>
2019-02-16 22:49:25 -0600

    Update staff.py

--- src/staff.py
+++ src/staff.py
@@ -2,6 +2,7 @@ import discord
 from discord.ext import commands
 from utils import checks
 from utils import config
+from clipboard import clip_message
 import datetime
 from datetime import datetime
 
@@ -217,41 +218,7 @@ class Staff:
         # The message
         msg = await self.bot.get_message(channel, message_id)
 
-        # Check for Message Already In
-        if config.on_clipboard(msg.id):
-            return
-
-        # Set the Clipboard Channel
-        clipboard = self.bot.get_channel(config.CHANNEL_ID_CLIPBOARD)
-
-        # have to add a check here to make sure its not the pin board itself
-        if channel == clipboard:
-            return
-
-        # Check if Message Author is in "No Clip" List
-        if msg.author in config.get_noclip_members():
-            return
-
-        # Setup Embed
-        embed = discord.Embed(colour=discord.Colour(0xca0003), description=msg.content, timestamp=datetime.now())
-
-        if msg.embeds:
-            data = msg.embeds[0]
-            if data.type == 'image':
-                embed.set_image(url=data.url)
-
-        if msg.attachments:
-            file = msg.attachments[0]
-            if file['url'].lower().endswith(('png', 'jpeg', 'jpg', 'gif', 'webp')):
-                embed.set_image(url=file['url'])
-            else:
-                embed.add_field(name='Attachment', value='[' + file['url'] + '](' + file['filename'] + ')', inline=False)
-
-        embed.set_author(name=msg.author.display_name, icon_url=msg.author.avatar_url)
-
-        clip_msg = await self.bot.send_message(clipboard, content="📋 - from {0.mention}".format(msg.channel), embed=embed)
-
-        config.add_clipboard(msg.id, clip_msg.id)
+        await clip_message(msg, self.bot)
 
     # COMMAND !addrole
     @commands.command(pass_context=True)
